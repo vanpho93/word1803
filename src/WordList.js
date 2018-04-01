4febf7e8bf133344f55a180882f3791d86abcb9e
Van Pho Nguyen <vanpho02@gmail.com>
2018-04-01 10:34:20 +0700

    add word form

--- src/WordList.js
+++ src/WordList.js
@@ -9,7 +9,8 @@ export class WordList extends Component {
                 { id: 'ab124', en: 'Two', vn: 'Hai', isMemorized: false },
                 { id: 'ab125', en: 'Three', vn: 'Ba', isMemorized: false },
                 { id: 'ab126', en: 'Four', vn: 'Bon', isMemorized: true },
-            ]
+            ],
+            shouldShowForm: false
         };
     }
 
@@ -18,32 +19,56 @@ export class WordList extends Component {
         this.setState({ words: newWords });
     }
 
+    toggleWord(id) {
+        const newWords = this.state.words.map(w => {
+            if (w.id !== id) return w;
+            return { ...w, isMemorized: !w.isMemorized };
+        });
+        this.setState({ words: newWords });
+    }
+
+    genListWord() {
+        const renderWord = word => (
+            <div className="word" key={word.id}>
+                <div className="word-container">
+                    <h3 className="text-success">{word.en}</h3>
+                    <h3 className="text-danger">
+                        { word.isMemorized ? '-----' : word.vn }
+                    </h3>
+                </div>
+                    <div className="btn-container">
+                    <button
+                        className="btn btn-success"
+                        onClick={() => this.toggleWord(word.id)}
+                    >
+                        { word.isMemorized ? 'Forgot' : 'Memorized' }
+                    </button>
+                    <button
+                        className="btn btn-warning"
+                        onClick={() => this.removeWord(word.id)}
+                    >
+                        Remove
+                    </button>
+                </div>
+            </div>
+        );
+        return this.state.words.map(renderWord);
+    }
+
     render() {
         return (
             <div>
-                {
-                    this.state.words.map(word => (
-                        <div className="word" key={word.id}>
-                            <div className="word-container">
-                                <h3 className="text-success">{word.en}</h3>
-                                <h3 className="text-danger">
-                                    { word.isMemorized ? '-----' : word.vn }
-                                </h3>
-                            </div>
-                                <div className="btn-container">
-                                <button className="btn btn-success">
-                                    { word.isMemorized ? 'Forgot' : 'Memorized' }
-                                </button>
-                                <button
-                                    className="btn btn-warning"
-                                    onClick={() => this.removeWord(word.id)}
-                                >
-                                    Remove
-                                </button>
-                            </div>
-                        </div>
-                    ))
-                }
+                <br />
+                <div className="form-group" style={{ width: '200px' }}>
+                    <input className="form-control" placeholder="English" />
+                    <br />
+                    <input className="form-control" placeholder="Vietnamese" />
+                    <br />
+                    <button className="btn btn-success">Add word</button>
+                    <button className="btn btn-danger">Cancel</button>
+                </div>
+                <button className="btn btn-success">Create new word</button>
+                { this.genListWord() }
             </div>
         );
     }
